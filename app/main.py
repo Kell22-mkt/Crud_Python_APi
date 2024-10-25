10c9470a44a2ce676755044ece2d706dd6b88a36
Kell22-mkt <kellyckaroline@gmail.com>
2024-10-25 18:07:30 -0300

    first commit

---
+++ app/main.py
@@ -0,0 +1,45 @@
+﻿from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+from typing import List
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/usuarios", response_model=List[schemas.UserInDB])
+def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return crud.get_users(db, skip=skip, limit=limit)
+
+@app.get("/usuarios/{id}", response_model=schemas.UserInDB)
+def read_user(id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+@app.post("/usuarios", response_model=schemas.UserInDB)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db=db, user=user)
+
+@app.put("/usuarios/{id}", response_model=schemas.UserInDB)
+def update_user(id: int, user: schemas.UserUpdate, db: Session = Depends(get_db)):
+    db_user = crud.update_user(db, user_id=id, user=user)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+@app.delete("/usuarios/{id}")
+def delete_user(id: int, db: Session = Depends(get_db)):
+    db_user = crud.delete_user(db, user_id=id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"message": "User deleted"}
---
+++ app/schemas.py
@@ -0,0 +1,18 @@
+﻿from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    nome: str
+    email: str
+    idade: int
+
+class UserCreate(UserBase):
+    pass
+
+class UserUpdate(UserBase):
+    pass
+
+class UserInDB(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
